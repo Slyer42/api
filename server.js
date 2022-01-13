d483e8cfac2787b0b26df0693ade7b6bb0c00e6f
Slyer42 <isufaj.daniel@web.de>
2022-01-13 15:10:05 +0100

    #nice

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(process.cwd() + '/vue-test/dist'));
 app.get('/', (req,res) => {
   res.sendFile(process.cwd() + '/vue-test/dist/index.html');
 });
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
